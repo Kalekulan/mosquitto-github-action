ceaf4bd58b2db500eadece9dac8943603968107b
Kalekulan <lukas.foughman@gmail.com>
2025-03-19 20:10:40 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,25 +7,32 @@ CERTIFICATES=$4
 CONFIG_DIR=$5
 PASSWORD_FILE=$6
 CONTAINERNAME=$7
+CONF_FILE=$CONFIG_DIR/mosquitto.conf
 
 echo "Certificates: $CERTIFICATES"
-echo "Config: $CONFIG"
+echo "Config_dir: $CONFIG_DIR"
 
 docker_run="docker run --detach --name $CONTAINERNAME"
 
+# Supports only one port as of now!!!
+echo "listener ${PORTS%%:*}" > $CONF_FILE
+
 for i in $(echo $PORTS | tr " " "\n")
 do
-  docker_run="$docker_run --publish $i"
+  docker_run="$docker_run --publish $i"  
 done
 
+# Add config volume
+docker_run="$docker_run --volume $CONFIG_DIR:/mosquitto/config/:ro"
 
 if [ -n "$CERTIFICATES" ]; then
   docker_run="$docker_run --volume $CERTIFICATES:/mosquitto-certs:ro"
 fi
 
-if [ -n "$CONFIG_DIR" ]; then
-  docker_run="$docker_run --volume $CONFIG_DIR:/mosquitto/config/:ro"
+if [ -n "$ANONYMOUS" ]; then
+  echo "allow_anonymous true" >> $CONF_FILE
 fi
+
 
 if [ -n "$PASSWORD_FILE" ]; then
   docker_run="$docker_run --volume $PASSWORD_FILE:/mosquitto/config/mosquitto.passwd:ro"
